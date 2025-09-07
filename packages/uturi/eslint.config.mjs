2d4ed722662a9016900e39279c325505ed942234
유동연 <ksr20612@daum.net>
2025-09-07 15:17:36 +0900

    fix(uturi): lint rule added
    - @uturi/sonification, @uturi/ui import/no-unresolved inactivated

--- packages/uturi/eslint.config.mjs
+++ packages/uturi/eslint.config.mjs
@@ -12,6 +12,13 @@ export default [
     },
     rules: {
       'react/no-unescaped-entities': 'off',
+      // @uturi/sonification, @uturi/ui 모듈 대응(TODO: 워크플로우에서 빌드 순서 명시할 것)
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: ['@uturi/sonification', '@uturi/ui'],
+        },
+      ],
     },
   },
   {
